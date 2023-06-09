f4fcb9c41da2b73443cbe671ff211abbd4c51c25
IdaRoos <ida_r@live.se>
2023-06-09 12:30:10 +0200

    Initial commit

---
+++ src/main/java/com/idaroos/projectclassroom/ProjectclassroomApplication.java
@@ -0,0 +1,13 @@
+package com.idaroos.projectclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectclassroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectclassroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idaroos/projectclassroom/ProjectclassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idaroos.projectclassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectclassroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
