d84f008c6b800a8db10921c6e5f7cbf1d8979e57
IdaRoos <ida_r@live.se>
2023-06-12 14:49:15 +0200

    Added methods to TeacherCourseServiceImpl

--- src/main/java/com/idaroos/projectclassroom/service/TeacherCourseServiceImpl.java
+++ src/main/java/com/idaroos/projectclassroom/service/TeacherCourseServiceImpl.java
@@ -3,13 +3,14 @@ package com.idaroos.projectclassroom.service;
 import com.idaroos.projectclassroom.dao.StudentCourseRepository;
 import com.idaroos.projectclassroom.dao.TeacherCourseRepository;
 import com.idaroos.projectclassroom.entity.StudentCourse;
+import com.idaroos.projectclassroom.entity.TeacherCourse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class TeacherCourseServiceImpl implements StudentCourseService{
+public class TeacherCourseServiceImpl implements TeacherCourseService{
 
     private TeacherCourseRepository teacherCourseRepository;
 
@@ -18,27 +19,30 @@ public class TeacherCourseServiceImpl implements StudentCourseService{
         this.teacherCourseRepository = teacherCourseRepository;
     }
     @Override
-    public void save(StudentCourse StudentCourse) {
+    public void save(TeacherCourse teacherCourse) {
+        teacherCourseRepository.save(teacherCourse);
 
     }
 
     @Override
-    public String update(StudentCourse studentCourse) {
-        return null;
+    public String update(TeacherCourse teacherCourse) {
+        teacherCourseRepository.save(teacherCourse);
+        return "Teacher's course was successfully updated.";
     }
 
     @Override
-    public void delete(StudentCourse studentCourse) {
+    public void delete(TeacherCourse teacherCourse) {
+teacherCourseRepository.delete(teacherCourse);
+    }
+
+    @Override
+    public List<TeacherCourse> findAllTeachersByCourseId(int id) {
+return teacherCourseRepository.findAllTeachersByCourseId(id);
 
     }
 
     @Override
-    public List<StudentCourse> findAllStudentsByCourseId(int id) {
-        return null;
-    }
-
-    @Override
-    public List<StudentCourse> findAllCoursesByUsername(String username) {
-        return null;
+    public List<TeacherCourse> findAllCoursesByUsername(String username) {
+return teacherCourseRepository.findAllCoursesByUsername(username);
     }
 }
